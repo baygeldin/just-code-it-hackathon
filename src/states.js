9c63270478fb1a0a910d7091bfd6faa0c17db665
Pochatkin <m.a.pochatkin@gmail.com>
2016-04-01 16:09:44 +0300

    kek

--- src/states.js
+++ src/states.js
@@ -1,2 +1,6 @@
 export const INIT = 'init'
 export const STARTED = 'started'
+export const CHOSE_1 = 'choose your lang'
+export const CHOSE_2 = 'choose preference lang'
+export const WAITING = 'waiting opponent'
+export const CHAT = 'chat'
